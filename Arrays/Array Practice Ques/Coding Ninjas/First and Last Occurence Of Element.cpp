6ea067dbb5b4707512eb9bc8ce33e5bed83a9c50
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-06-09 16:59:25 +0530

    Ques With Pair function usage first time

--- Arrays/Array Practice Ques/Coding Ninjas/First and Last Occurence Of Element.cpp
+++ Arrays/Array Practice Ques/Coding Ninjas/First and Last Occurence Of Element.cpp
@@ -1,45 +1,120 @@
-#include<iostream>
+#include <iostream>
+#include<vector>
 using namespace std;
+int firstOcc(vector<int>& arr, int n, int key) {
 
-int FirstOcc(int arr[], int n, int key){
-    int s=0, e=n-1;
-    int mid=s+(e-s)/2;
-    int ans=-1;
-    while(s<=e){
-        if (arr[mid]==key){
-            ans=mid;
-            e=mid-1;
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
         }
-        else if(key>arr[mid]){
-            s=mid+1;
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
         }
-        else if(key<arr[mid]){
-            s=mid-1;
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
         }
+
+        mid = s + (e-s)/2;
     }
     return ans;
 }
 
-int LastOcc(int arr[], int n, int key){
-    int s=0, e=n-1;
-    int mid=s+(e-s)/2;
-    int ans=-1;
-    while(s<=e){
-        if (arr[mid]==key){
-            ans=mid;
-            s=mid+1;
+int lastOcc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
         }
-        else if(key>arr[mid]){
-            s=mid+1;
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
         }
-        else if(key<arr[mid]){
-            s=mid-1;
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
         }
+
+        mid = s + (e-s)/2;
     }
     return ans;
 }
-int main(){
-    int even[6]={1,2,3,3,5,6};
-    cout<<"First Occ: "<<FirstOcc(even,6,3);
-    cout<<"Last Occ: "<<LastOcc(even,6,3);
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    pair<int,int> p;
+    p.first = firstOcc(arr, n, k);
+    p.second = lastOcc(arr, n, k);
+    
+    return p;
 }
+
+
+
+//To Find Total No of Occurences -- return lastOcc-firstOcc + 1
+
+
+
+
+
+// int FirstOcc(int arr[], int n, int key)
+// {
+//     int s = 0, e = n - 1;
+//     int mid = s + (e - s) / 2;
+//     int ans = -1;
+//     while (s <= e)
+//     {
+//         if (arr[mid] == key)
+//         {
+//             ans = mid;
+//             e = mid - 1;
+//         }
+//         else if (key > arr[mid])
+//         {
+//             s = mid + 1;
+//         }
+//         else if (key < arr[mid])
+//         {
+//             s = mid - 1;
+//         }
+//     }
+//     return ans;
+// }
+
+// int LastOcc(int arr[], int n, int key)
+// {
+//     int s = 0, e = n - 1;
+//     int mid = s + (e - s) / 2;
+//     int ans = -1;
+//     while (s <= e)
+//     {
+//         if (arr[mid] == key)
+//         {
+//             ans = mid;
+//             s = mid + 1;
+//         }
+//         else if (key > arr[mid])
+//         {
+//             s = mid + 1;
+//         }
+//         else if (key < arr[mid])
+//         {
+//             s = mid - 1;
+//         }
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     int even[6] = {1, 2, 3, 3, 5, 6};
+//     cout << "First Occ: " << FirstOcc(even,6,3);
+//     cout << "Last Occ: " << LastOcc(even, 6, 3);
+// }
+
