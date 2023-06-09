8fb8143a78bd36639c475fd4787cd2db5904c79e
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-06-10 01:10:10 +0530

    Update Sqrt(x).cpp

--- Arrays/Array Practice Ques/Coding Ninjas/Sqrt(x).cpp
+++ Arrays/Array Practice Ques/Coding Ninjas/Sqrt(x).cpp
@@ -1,37 +1,55 @@
 #include<iostream>
 using namespace std;
 
-long long int BinarySearch(int n){
-    if (n==0){
-        return n;
+ long long int sqrtInteger(int n) {
+        
+        int s = 0;
+        int e = n;
+        long long int mid = s + (e-s)/2;
+        
+        long long int ans = -1;
+        while(s<=e) {
+            
+            long long int square = mid*mid;
+            
+            if(square == n)
+                return mid;
+            
+            if(square < n ){
+                ans = mid;
+                s = mid+1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
     }
-    int s=0;
-    int e=n;
-    long long int mid=s+(e-s)/2;
-    if (n==0 || n<1){
-        return 0;
-    }
-    long long int ans=-1;
-    while(s<=e){
-        long long int square=mid*mid;
-        if (square==n){
-            return mid;
+
+double morePrecision(int n, int precision, int tempSol) {
+    
+    double factor = 1;
+    double ans = tempSol;
+
+    for(int i=0; i<precision; i++) {
+        factor = factor/10;
+
+        for(double j=ans; j*j<n; j= j+factor ){
+            ans = j;
         }
-        else if(square<n){
-            ans=mid;
-            s=mid+1;
-        }
-        else{
-            e=mid-1;
-        }
-        mid=s+(e-s)/2;
     }
     return ans;
-    
 }
-    int mySqrt(int x) {
-        return BinarySearch(x);
-    }
-int main(){
-    mySqrt(10);
+
+int main() {
+    int n;
+    cout <<" Enter the number " << endl;
+    cin >> n;
+
+    int tempSol = sqrtInteger(n);
+    cout <<" Answer is " << morePrecision(n, 3, tempSol) << endl;
+
+    return 0;
 }
