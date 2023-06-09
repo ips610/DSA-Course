77a1bb75e4f1a470ce2560b537947e2ae347676c
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-06-09 16:43:11 +0530

    Ques

--- Arrays/Array Practice Ques/Coding Ninjas/First and Last Occurence Of Element.cpp
+++ Arrays/Array Practice Ques/Coding Ninjas/First and Last Occurence Of Element.cpp
@@ -1,6 +1,45 @@
 #include<iostream>
 using namespace std;
 
+int FirstOcc(int arr[], int n, int key){
+    int s=0, e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if (arr[mid]==key){
+            ans=mid;
+            e=mid-1;
+        }
+        else if(key>arr[mid]){
+            s=mid+1;
+        }
+        else if(key<arr[mid]){
+            s=mid-1;
+        }
+    }
+    return ans;
+}
+
+int LastOcc(int arr[], int n, int key){
+    int s=0, e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if (arr[mid]==key){
+            ans=mid;
+            s=mid+1;
+        }
+        else if(key>arr[mid]){
+            s=mid+1;
+        }
+        else if(key<arr[mid]){
+            s=mid-1;
+        }
+    }
+    return ans;
+}
 int main(){
-    
+    int even[6]={1,2,3,3,5,6};
+    cout<<"First Occ: "<<FirstOcc(even,6,3);
+    cout<<"Last Occ: "<<LastOcc(even,6,3);
 }
