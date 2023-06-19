b7f7843cd9dd825ba6324603ad37fbf5d0c9892f
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-06-19 12:34:13 +0530

    Bubble Sort

--- Arrays/Bubble Sort.cpp
+++ Arrays/Bubble Sort.cpp
@@ -4,4 +4,16 @@ using namespace std;
 
 int main(){
     vector<int> arr={1,3,9,0,8,16,2};
+    int n=arr.size();
+
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if (arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
 }
