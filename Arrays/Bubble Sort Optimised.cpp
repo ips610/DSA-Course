ac894641f7d9a496a4281dc9ecb4302ed6af3054
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-06-19 12:48:44 +0530

    Optimised Bubble Sort

--- Arrays/Bubble Sort Optimised.cpp
+++ Arrays/Bubble Sort Optimised.cpp
@@ -6,8 +6,8 @@ int main(){
     vector<int> arr={1,3,9,0,8,16,2};
     int n=arr.size();
 
-    for (int i=1;i<n;i++){
-        for(int j=0;j<n-1;j++){
+    for (int i=1;i<n;i++){  //if i=0;i<n-1 then j=0; j<n-i-1
+        for(int j=0;j<n-i;j++){
             if (arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
