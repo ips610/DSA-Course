ddeb8e6464baae41d62327eb3f06df9ebfe05190
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-11-29 08:29:59 +0530

    Update 1. Heap Intro.cpp

--- Heaps/1. Heap Intro.cpp
+++ Heaps/1. Heap Intro.cpp
@@ -65,6 +65,25 @@ class heap{
             }
         }
     }
+
+    void heapify(int arr[], int n, int i){
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i + 1;
+
+        if (left < n && arr[largest] < arr[left]){
+            largest = left;
+        }
+
+        if (right < n && arr[largest] < arr[right]){
+            largest = right;
+        }
+
+        if (largest != i){
+            swap(arr[largest], arr[i]);
+            heapify(arr, n, largest);
+        }
+    }
 };
 
 int main(){
