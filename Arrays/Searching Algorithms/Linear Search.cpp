c6d4aea5c04fc1e32c68673f349eda2c664bc2ba
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2024-01-09 20:14:06 +0530

    something

--- Arrays/Searching Algorithms/Linear Search.cpp
+++ Arrays/Searching Algorithms/Linear Search.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int arr[10]={1,3,5,2,9,11,7,0,8,6};
-    int key = 6;
+void LinearSearch(int *arr,int key){
     for (int i = 0;i<10;i++){
         if (key==arr[i]){
             cout<<"Index is: "<<i<<endl;
         }
     }
+}
+
+// *arr == arr[0]
+int main(){
+    int arr[10]={1,3,5,2,9,11,7,0,8,6};
+    int key = 6;
+    // for (int i = 0;i<10;i++){
+    //     if (key==arr[i]){
+    //         cout<<"Index is: "<<i<<endl;
+    //     }
+    // }
+    LinearSearch(arr,key);
 }
